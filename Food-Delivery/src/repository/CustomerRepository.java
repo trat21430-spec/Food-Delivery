ace9b8586d13f3252fa42940f878e6428d46456e
vothaonguyen222-collab <vothaonguyen222@gmail.com>
2026-09-21 09:45:14 +0700

    Update CustomerRepository.java

--- Food-Delivery/src/repository/CustomerRepository.java
+++ Food-Delivery/src/repository/CustomerRepository.java
@@ -8,6 +8,33 @@ package repository;
  *
  * @author HP
  */
-public class CustomerRepository {
-    
+
+import model.Customer;
+
+public class CustomerRepository extends CsvRepository<Customer> {
+
+    public CustomerRepository(String filePath) {
+        super(filePath);
+    }
+
+    @Override
+    protected Customer fromCsvLine(String line) {
+        String[] parts = line.split(",");
+        Customer customer = new Customer();
+        customer.setId(parts[0].trim());
+        customer.setName(parts[1].trim());
+        customer.setPhone(parts[2].trim());
+        customer.setAddress(parts[3].trim());
+        return customer;
+    }
+
+    @Override
+    protected String toCsvLine(Customer customer) {
+        return String.join(",", customer.getId(), customer.getName(), customer.getPhone(), customer.getAddress());
+    }
+
+    @Override
+    protected String getHeader() {
+        return "id,name,phone,address";
+    }
 }
