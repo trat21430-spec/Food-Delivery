99e6348288d3e2499ceec555fca875b3763b7dc8
vothaonguyen222-collab <vothaonguyen222@gmail.com>
2026-09-21 09:45:21 +0700

    Update OrderItemRepository.java

--- Food-Delivery/src/repository/OrderItemRepository.java
+++ Food-Delivery/src/repository/OrderItemRepository.java
@@ -8,6 +8,35 @@ package repository;
  *
  * @author HP
  */
-public class OrderItemRepository {
-    
+
+import model.OrderItem;
+
+public class OrderItemRepository extends CsvRepository<OrderItem> {
+
+    public OrderItemRepository(String filePath) {
+        super(filePath);
+    }
+
+    @Override
+    protected OrderItem fromCsvLine(String line) {
+        String[] parts = line.split(",");
+        OrderItem item = new OrderItem();
+        item.setId(parts[0].trim());
+        item.setOrderId(parts[1].trim());
+        item.setMenuItemId(parts[2].trim());
+        item.setQuantity(Integer.parseInt(parts[3].trim()));
+        item.setPrice(Double.parseDouble(parts[4].trim()));
+        return item;
+    }
+
+    @Override
+    protected String toCsvLine(OrderItem item) {
+        return String.join(",", item.getId(), item.getOrderId(), item.getMenuItemId(),
+                String.valueOf(item.getQuantity()), String.valueOf(item.getPrice()));
+    }
+
+    @Override
+    protected String getHeader() {
+        return "id,orderId,menuItemId,quantity,price";
+    }
 }
